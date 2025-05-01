a6725db1375362ff5dec0b9b53653d80f6806003
ayush229 <53784806+ayush229@users.noreply.github.com>
2025-05-01 23:31:33 +0530

    Update auth.js

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,13 +1,13 @@
 export const isAuthenticated = () => {
-  const auth = localStorage.getItem("auth");
+  const auth = localStorage.getItem("token"); // <-- Changed to "token"
   return !!auth;
 };
 
 export const getAuthHeader = () => {
-  const auth = localStorage.getItem("auth");
+  const auth = localStorage.getItem("token"); // <-- Changed to "token"
   return auth ? `Basic ${auth}` : null;
 };
 
 export const logout = () => {
-  localStorage.removeItem("auth");
+  localStorage.removeItem("token"); // <-- Changed to "token"
 };
