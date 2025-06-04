a6db7d4297f5ee5c8cc5c80f763cc19d73b45653
ayush229 <53784806+ayush229@users.noreply.github.com>
2025-06-04 15:47:01 +0530

    Update CreateAgentPage.jsx

--- src/pages/CreateAgentPage.jsx
+++ src/pages/CreateAgentPage.jsx
@@ -30,7 +30,7 @@ const CreateAgentPage = () => {
 
     try {
       const response = await axios.post(
-        "https://web-scraper-api-production-ec96.up.railway.app/scrape_and_store",
+        "https://web-scrap-scrapy-production.up.railway.app/scrape_and_store",
         {
           agent_name: agentName,
           url: urls,
